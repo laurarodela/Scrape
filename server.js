8e6a999133e34848b72136ab0fee186bfacaadb6
Laura Rodela <laurarodela@Lauras-MacBook-Pro-2.local>
2020-04-11 09:08:28 -0700

    new scapre files with installs

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
